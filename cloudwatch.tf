42188de879ca9e8cc235ccf05d53d96120bc6722
Graham Moucka <mouckatron@gmail.com>
2020-12-21 23:13:21 +0000

    fix variable name

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -3,7 +3,7 @@ resource "aws_cloudwatch_event_rule" "load-url" {
   name        = "${var.appname}-load-url"
   description = "Periodically load ${var.appname} url"
 
-  schedule_expression = "rate(${var.every_x_mins} minutes)"
+  schedule_expression = "rate(${var.every_x_minutes} minutes)"
 }
 
 resource "aws_cloudwatch_event_target" "load-url-lambda" {
