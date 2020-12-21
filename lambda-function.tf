784a9fca63b25d5637a757a083a8893ec42e8494
Graham Moucka <mouckatron@gmail.com>
2020-12-21 23:12:09 +0000

    use module path

--- lambda-function.tf
+++ lambda-function.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_function" "load-url" {
   filename         = "lambda.zip"
   handler          = "main.lambda_handler"
   runtime          = "python3.6"
-  source_code_hash = filebase64sha256("lambda.zip")
+  source_code_hash = filebase64sha256("${path.module}/lambda.zip")
 
   role = aws_iam_role.lambda.arn
 
